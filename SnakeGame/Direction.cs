f1a5a37c1de45d834fc994ef7634e2bfaf7f9773
Paweł Najdecki <rystor69@gmail.com>
2024-10-15 22:48:25 +0200

    Create project, add Assets and .gitignore

---
+++ SnakeGame/Direction.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakeGame
+{
+    public class Direction
+    {
+        public int RowOffset { get; }
+        public int ColumnOffset { get; }
+
+        private Direction(int rowOffset, int columnOffset)
+        {
+            RowOffset = rowOffset;
+            ColumnOffset = columnOffset;
+        }
+    }
+}
